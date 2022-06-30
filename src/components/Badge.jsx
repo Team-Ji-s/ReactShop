300ace9c6ac4e2611b13c8dc2421ae5c522f3a76
Lim-Ji-Hyeon <sen03620@naver.com>
2022-06-30 22:50:48 +0900

    Design : 뱃지 컴포넌트 UI 수정

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,20 +1,26 @@
-import styled from "styled-components";
+import styled from "styled-components"
 
-export default function Badge({type, children}) {
-  return (
-    <StyledBadge type={type}>{children}</StyledBadge>
-  )
+export default function Badge({ type }) {
+  if (type === "new") {
+    return <span className="badge badge-lg badge-success">NEW</span>
+  } else if (type === "best") {
+    return <span className="badge badge-lg badge-error">BEST</span>
+  }
 }
 
 const StyledBadge = styled.span`
-  width : ${({theme}) => theme.width.small};
-  height : ${({theme}) => theme.height.small};
-  background-color : ${({theme, type}) => type==="new" ? theme.color.green : theme.color.red};
-  color : ${({theme}) => theme.color.white};
-  display : inline-block;
-  font-size : ${({theme}) => theme.font.size.normal};
-  text-align : center;
-  border : 0;
-  border-radius : ${({theme}) => theme.borderRadius.all};
-  font-weight : ${({theme}) => theme.font.weight.normal};
+  width: ${({ theme }) => theme.width.small};
+  height: ${({ theme }) => theme.height.small};
+  background-color: ${({ theme, type }) => (type === "new" ? theme.color.green : theme.color.red)};
+  color: ${({ theme }) => theme.color.white};
+  display: inline-block;
+  font-size: ${({ theme }) => theme.font.size.normal};
+  text-align: center;
+  border: 0;
+  border-radius: ${({ theme }) => theme.borderRadius.all};
+  font-weight: ${({ theme }) => theme.font.weight.normal};
+`
+
+const BadgeWrapper = styled.h2`
+  display: inline-block;
 `
