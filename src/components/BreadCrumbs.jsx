6482ce6499c90d7c9c195c2c516e24a5ec618824
Lim-Ji-Hyeon <sen03620@naver.com>
2022-07-05 21:48:05 +0900

    Refactor : 카테고리 관련 함수 추가

--- src/components/BreadCrumbs.jsx
+++ src/components/BreadCrumbs.jsx
@@ -1,30 +1,46 @@
-import React from "react";
-import styled from "styled-components";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import React from "react"
+import styled from "styled-components"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons"
 
-export default function BreadCrumbs({from, to}) {
-  return ( 
+export default function BreadCrumbs({ from, to }) {
+  let category = ""
+  switch (from) {
+    case "electronics":
+      category = "디지털"
+      break
+    case "men's clothing":
+    case "women's clothing":
+      category = "패션"
+      break
+    case "jewelery":
+      category = "악세서리"
+      break
+    default:
+      category = "홈"
+      break
+  }
+  return (
     <BreadCrumbsWrapper>
-      {from}
+      {category}
       <Arrow icon={faAngleRight}></Arrow>
       {to}
     </BreadCrumbsWrapper>
   )
 }
 
-
 const BreadCrumbsWrapper = styled.span`
-  height : ${({theme}) => theme.height.small};
-  font-size : ${({theme}) => theme.font.size.large};
-  color : ${({theme}) => theme.color.black};
-  font-weight : ${({theme}) => theme.font.weight.normal};
-  display : inline-block;
-  margin : 2rem;
+  font: "Roboto", sans-serif;
+  height: ${({ theme }) => theme.height.small};
+  font-size: ${({ theme }) => theme.font.size.large};
+  color: ${({ theme }) => theme.color.black};
+  font-weight: ${({ theme }) => theme.font.weight.normal};
+  display: inline-block;
+  margin: 2rem;
 `
 
 const Arrow = styled(FontAwesomeIcon)`
-  width : ${({theme}) => theme.font.size.normal};
-  height : ${({theme}) => theme.font.size.normal};
-  margin : 0 0.5rem 0 0.5rem;
+  width: ${({ theme }) => theme.font.size.normal};
+  height: ${({ theme }) => theme.font.size.normal};
+  margin: 0 0.5rem 0 0.5rem;
 `
